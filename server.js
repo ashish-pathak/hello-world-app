cc6c4cc0e175608fa2ec2f233c4065710efb4cd3
Ashish <pathakashish2008@gmail.com>
2025-07-24 17:40:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -13,13 +13,14 @@ app.prepare().then(() => {
     // This tells it to parse the query portion of the URL.
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
-
+    const apiKey = "KhGcN464,K64u6y464RFWSs364646^&4Gfj";
     if (pathname === '/a') {
       app.render(req, res, '/a', query)
     } else if (pathname === '/b') {
       app.render(req, res, '/b', query)
     } else {
-      handle(req, res, parsedUrl)
+      handle(req, res, parsedUrl);
+      
     }
   }).listen(port, (err) => {
     if (err) throw err
